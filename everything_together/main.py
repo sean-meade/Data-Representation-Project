fd4988e383828d06694782f6352ed4c2567028b9
Sean Meade <seanpatmeade@gmail.com>
2020-12-24 10:47:59 +0000

    comments added to code

--- everything_together/main.py
+++ everything_together/main.py
@@ -2,64 +2,64 @@ from flask import Flask, redirect, url_for, render_template, request, make_respo
 import json
 from flask_jsglue import JSGlue
 import requests
+import urllib3
 import datetime
 from bs4 import BeautifulSoup
+urllib3.disable_warnings()
 
 app = Flask(__name__)
 jsglue = JSGlue(app)
 
 @app.route("/")
 def home():
-    resp = make_response(render_template("index.html"))
-    # Set a same-site cookie for first-party contexts
-    # Ensure you use "add" to not overwrite existing cookie headers
-    # Set a cross-site cookie for third-party contexts
     
-    resp.headers.add('Set-Cookie','cookie2=value2; SameSite=None; Secure')
-    return resp
+    return render_template("index.html")
 
 @app.route("/weather_link", methods=["POST"])
 def weather_request():
+    # Check for incoming post and collect data from url in the form of xml
     if request.method == "POST":
         data = {}
         data = request.json
-        response = requests.get(data['url'], verify=False)
+        response = requests.get(data['url'])
         soup = BeautifulSoup(response.text, 'xml')
  
+        # collect date and time
         date= data['date']
         time= data['time']
 
+        # Find the weather information for the date and time you're interested in
         result = soup.find_all("time", {"from": date + "T" + time + ":00Z"})
 
+        # convert the input date and time to a python datetime
+        """
+        This is because Met Éireann stores weather information in 3 hour intervals
+        after 85 hours and in 6 hour intervals after 144 hours.
+        """
         date_for_python = datetime.datetime.strptime(date + ' ' + time + ":00", '%Y-%m-%d %H:%M:%S')
         difference = abs((date_for_python - datetime.datetime.now()))
         seconds_in_day = 24 * 60 * 60
         mins, secs = divmod(difference.days * seconds_in_day + difference.seconds, 60)
+        # So you check how many hours are there between today and when the user requests the info for
         hours = mins/60
   
-        if result == [] and hours > 85 and hours < 144:
-            time = int(time[slice(2)])
-            while time % 3 != 0:
-                time = time - 1
-            time = str(time)
-            if len(time) == 1:
-                time = "0" + time + ":00"
-            else:
-                time = str(time) + ":00"
-            result = soup.find_all("time", {"from": date + "T" + time + ":00Z"})
-        elif result == [] and hours > 144:
-            time = int(time[slice(2)])
-            while time % 6 != 0:
-                time = time - 1
-            time = str(time)
-            if len(time) == 1:
-                time = "0" + time + ":00"
-            else:
-                time = str(time) + ":00"
-            result = soup.find_all("time", {"from": date + "T" + time + ":00Z"})
-        else:
-            result = soup.find_all("time", {"from": date + "T" + time + ":00Z"})
 
+        time = int(time[slice(2)])
+        # after 90 hours weaether info is given in 3 hour intervals and after 
+        # 144 they are given in 6 hour intervals. I check for the nearest 
+        # weather information if this is the case
+        while result == []:
+            time = time - 1
+            time = str(time)
+            if len(time) == 1:
+                time = "0" + time + ":00"
+            else:
+                time = str(time) + ":00"
+            result = soup.find_all("time", {"from": date + "T" + time + ":00Z"})
+            time = int(time[slice(2)])
+        
+        
+        # Create dictionary and fill relevant information along with key
         weather_dict = {}
         temperature = result[0].temperature['value']
         weather_dict["Temperature: "] = temperature
@@ -71,19 +71,12 @@ def weather_request():
         weather_dict["Humidity: "] = humidity
         pressure = result[0].pressure['value']
         weather_dict["Pressure: "] = pressure
-
-        if len(str(result[1].precipitation)) > 55:
-            precipitationMax = result[1].precipitation['maxvalue']
-            weather_dict["Precipitation Max: "] = precipitationMax
-            precipitationMin = result[1].precipitation['minvalue']
-            weather_dict["Precipitation Min: "] = precipitationMin
-        else:
-            precipitation = result[1].precipitation['value']
-            weather_dict["Precipitation: "] = precipitation
+        precipitationMax = result[1].precipitation['maxvalue']
+        weather_dict["Precipitation Max: "] = precipitationMax
+        precipitationMin = result[1].precipitation['minvalue']
+        weather_dict["Precipitation Min: "] = precipitationMin
 
         return weather_dict
-    else:
-        return render_template('<h1>Nope</h1>')
 
 if __name__ == "__main__":
     app.run(debug=True)
--- everything_together/static/route.js
+++ everything_together/static/route.js
@@ -1,65 +1,64 @@
+// set API accessToken for MapBox
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2Vhbi1tZWFkZSIsImEiOiJja2oxcDdlNG4wOHRtMnJwaThqaXNsb3c0In0.b3gF1oc--lsBuTigpSurDg';
 
+// Initialize a map with the center being an view of Ireland
 const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
-    center: [147.101601, -43.245012],
-    zoom: 13
+    center: [-7.266155, 53.750140],
+    zoom: 4.75
 });// map variable
 
-function craeteAutoPoints(dist, div, center) {
-    // Takes a distance and a divide to give the radius of a circle of 8 points based around the center
-    var longConvert = dist/54.6; // dist_in_long
-    var latConvert = dist/69; // dist_in_lat
-
-    var x = center[0]; //current_long
-    var y = center[1]; //currnt_lat
-
-    var coord_i = longConvert/Math.sqrt(Math.pow(dist, 2) * 2);
-    var coord_j = latConvert/Math.sqrt(Math.pow(dist, 2) * 2);
-
-    var circle_points = [[x, y -latConvert/div], 
-                        [x + coord_i, y + coord_j],
-                        [x + longConvert/div, y],
-                        [x + coord_i, y - coord_j],
-                        [x, y -latConvert/div],
-                        [x - coord_i, y - coord_j],
-                        [x -longConvert/div, y],
-                        [x - coord_i, y + coord_j]];
-
-    return circle_points;
-}
-
+// function ran when routeButton is clicked
 function setupMap() {
 
-    navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {enableHighAccuracy: true
-    });
+    // Get current geo location position
+    navigator.geolocation.getCurrentPosition(successLocation, 
+                                                errorLocation, 
+                                                {enableHighAccuracy: true});
 
+    // alert if no location found
     function errorLocation(){
         alert("Could not find location.");
     }
 
+    // run function with position as current geo location
     function successLocation(position){
-        
+
+        // Set all the higher level variables 
+        // get long and lat (has to be in this order for MapBox)
         const currentLoc = [position.coords.longitude, position.coords.latitude];
+        // The input distance (if there is one)
         var inputDist = parseFloat(document.getElementById("distance").value);
+        // A list to track clicks (if in click route mode)
         var clickRoute = [currentLoc];
+        // A list to track points in auto route mode
         var autoRoute = [currentLoc];
+        // an empty geoJSON feature collection 
         var nothing = turf.featureCollection([]);
+        // starting divide variable (I'm trying to fine tune the auto route method)
         var startDivide = 3;
+        // a geoJSON feature collection to track clicks to add markers to map
         var clicks = turf.featureCollection([]);
+        // used again in the creation of thr auto route
         var index = -1;
 
+        // initialize a new map zoomed in on current location
         const map = new mapboxgl.Map({
             container: 'map',
             style: 'mapbox://styles/mapbox/streets-v11',
             center: currentLoc,
             zoom: 13
-        });// map variable
+        });
 
+        // a geoJSON feature collection representing current location
         var geoJSON_currentLoc = turf.featureCollection([turf.point(currentLoc)]);
 
+        // function that executes only after the map has loaded
         map.on('load', function() {
+
+            // create a layer over the map the represents the 
+            // current location as a small circle
             map.addLayer({
                 id: 'currentLoc',
                 type: 'circle',
@@ -68,45 +67,50 @@ function setupMap() {
                     type: 'geojson'
                 },
                 paint: {
-                    'circle-radius': 20,
+                    'circle-radius': 10,
                     'circle-color': 'white',
                     'circle-stroke-color': '#3887be',
                     'circle-stroke-width': 3,
                 }
-            }); // add layer of current location
+            });
 
+            // create a source to add to for the route but
+            // initially set to nothing
             map.addSource('route', {
                 type: 'geojson',
                 data: nothing
-              });
+            });
               
-              map.addLayer({
+            // Add a layer to the map to represent the route
+            // giving 'route' as the source data
+            map.addLayer({
                 id: 'routeline-active',
                 type: 'line',
                 source: 'route',
                 layout: {
-                  'line-join': 'round',
-                  'line-cap': 'round'
+                    'line-join': 'round',
+                    'line-cap': 'round'
                 },
                 paint: {
-                  'line-color': '#3887be',
-                  'line-width': [
-                    "interpolate",
-                    ["linear"],
-                    ["zoom"],
-                    12, 3,
-                    22, 12
-                  ]
+                    'line-color': '#3887be',
+                    'line-width': [
+                        "interpolate",
+                        ["linear"],
+                        ["zoom"],
+                        12, 3,
+                        22, 12
+                    ]
                 }
-              }, 'waterway-label'); // add layer to be used for route
+                }, 'waterway-label');
 
-              
-              map.addLayer({
+              // Add layer that will be used to mark clicks as dots
+              // for the click route
+            map.addLayer({
                 id: 'click-marker',
                 type: 'symbol',
                 source: {
-                  data: clicks,
-                  type: 'geojson'
+                    data: clicks,
+                    type: 'geojson'
                 },
                 layout: {
                   'icon-allow-overlap': true,
@@ -114,18 +118,19 @@ function setupMap() {
                   'icon-image': 'marker-15',
                 }
               
-              });
+            });
 
+            // if click route is chosen
+            if (document.getElementById("dist").style.visibility == "hidden") {
 
-              if (document.getElementById("dist").style.visibility == "hidden") {
-
+                // when the map is clicked with the long and lat of click
                 map.on('click', function(e) {
 
                     var coords = e.lngLat;
-
                     var click = [coords.lng, coords.lat]
                     clickRoute.push(click); // updates clickRoute with new point
                     
+                    // set the click as a geoJSON feature
                     var pt = turf.point(
                         [click[0], click[1]],
                         {
@@ -134,91 +139,119 @@ function setupMap() {
                         }
                     );
                     
-                    clicks.features.push(pt);
+                    clicks.features.push(pt); // updates clickRoute with new geoJSON feature
 
-                    createRouteRequest(clickRoute);
+                    // create route and update distance of it
+                    createRoute(clickRoute);
+                    // Add click to make a marker on the map
                     addMarker(clicks);
                 }); // map on click
     
-            } else {
-                // get input value
-                
-    
+            } else {    
                 // check if inputDist is a number
                 if (inputDist > 0){
-
-                    
-                    
-                    addPoint(inputDist, startDivide);
+                    // use the input distance to create route
+                    createAutoRoute(inputDist, startDivide);
                     
                 } else {
+                    // tells user to input a number
                     alert("Please select a distance.");
                 }
     
             }// if/else if for toggle of click or auto
         }); // map on load
 
+        // function that simply adds a marker where clicked
         function addMarker(points) {
             map.getSource('click-marker')
                     .setData(points);
         }
 
+        // create auto route
+        function createAutoRoute(dist, divideInput) {
+            // index to add points from the circle points
+            index = index + 1;
 
-    function addPoint(dist, divideInput) {
-        index = index + 1;
-
-        if (index < 8){
-
-        var circle_points = craeteAutoPoints(dist, divideInput, currentLoc);
-
-        autoRoute.push(circle_points[index]);
-
-        createRouteRequest(autoRoute);
-        } else {
+            // circle points only has 8 points
+            if (index < 8){
             
-                alert("Cannot find suitable loop.");
+                // save the output of  the created points
+                var circle_points = craeteAutoPoints(dist, divideInput, currentLoc);
 
+                // add the next point to the autoRoute
+                autoRoute.push(circle_points[index]);
+
+                // create route with above
+                createRoute(autoRoute);
+            } else {
+                // throws alert if a route can't be found with the 8 points
+                alert("Sorry cannot find suitable route (still trying to work out the kinks), try using the click route option.");
+            }
         }
-    }
         
-        function createRouteRequest(route){
+        // create a route with given lat long values 
+        function createRoute(route){
 
-
-            // console.log(route.join(';'));
+            // create url to make request with
             var url = 'https://api.mapbox.com/optimized-trips/v1/mapbox/driving/'+ route.join(';') + '?&overview=full&steps=true&geometries=geojson&source=first&access_token=' + mapboxgl.accessToken;
             
+
             $.ajax({
                 method: 'GET',
                 url: url,
-              }).done(function(data) {
-                // Create a GeoJSON feature collection
+            }).done(function(data) {
+                // Create a GeoJSON feature collection containing the route
                 var routeGeoJSON = turf.featureCollection([turf.feature(data.trips[0].geometry)]);
+                // grab the distance of the route
                 var distanceObject = turf.featureCollection([turf.feature(data.trips[0].distance)]);
-
                 var distance = distanceObject["features"][0]["geometry"]
-                console.log(distance);
-                console.log(inputDist * 1609.34, "distance wated");
-                console.log(inputDist * 1609.34 * 1.25, "distance hign");
-                console.log(inputDist * 1609.34 * 0.75, "distance low");
-
+           
+                // if the user is using clicks to create route
                 if (document.getElementById("dist").style.visibility == "hidden"){
+                    // Display the distance of the route
                     document.getElementById("miles").innerText = (distance/1609.34).toFixed(2);
+                    // show route on map
                     map.getSource('route')
-                    .setData(routeGeoJSON);
+                        .setData(routeGeoJSON);
                 } else {
+                    // check to see if the auto route produces a roue with a distance with 25% of input
+                    // distance (yes that's how bad it is it doesn't even work most of the time)
                     if (distance <= inputDist * 1609.34 * 1.25 && distance >= inputDist * 1609.34 * 0.75) {
+                        // Display the distance of the route
                         document.getElementById("miles").innerText = (distance/1609.34).toFixed(2);
-                    map.getSource('route')
-                    .setData(routeGeoJSON);
+                        // show route on map
+                        map.getSource('route')
+                          .setData(routeGeoJSON);
                     } else {
-                        addPoint(inputDist, startDivide);
+                        // otherwise run the create Auto route again with the next point
+                        createAutoRoute(inputDist, startDivide);
                     }
-
-                }
-                
-              });  
+                } 
+            });  
         }
-        
 
+        function craeteAutoPoints(dist, div, center) {
+            // This function creates a circle of points around the center
+            // with radius longConvert or latConvert depending on the direction
+            
+            var longConvert = dist/54.6; // dist_in_long
+            var latConvert = dist/69; // dist_in_lat
+        
+            var x = center[0]; //current_long
+            var y = center[1]; //currnt_lat
+        
+            var coord_i = longConvert/Math.sqrt(Math.pow(dist, 2) * 2);
+            var coord_j = latConvert/Math.sqrt(Math.pow(dist, 2) * 2);
+        
+            var circle_points = [[x, y -latConvert/div], 
+                                [x + coord_i, y + coord_j],
+                                [x + longConvert/div, y],
+                                [x + coord_i, y - coord_j],
+                                [x, y -latConvert/div],
+                                [x - coord_i, y - coord_j],
+                                [x -longConvert/div, y],
+                                [x - coord_i, y + coord_j]];
+            return circle_points;
+        }
     }// successLocation
 }
